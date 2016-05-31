2bb0337d79d04883556c33b01ec6d5d241a87061
Brian Steere <brian.steere@smartthings.com>
2016-05-31 07:58:25 -0500

    Remove unnecessary things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-import { RouteConfig, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 
 import { ApiService } from './shared';
-import { HomeComponent } from './home';
-import { AboutComponent } from './about';
 
 import '../style/app.scss';
 
@@ -12,19 +9,15 @@ import '../style/app.scss';
  * Top Level Component
  */
 @Component({
-  selector: 'my-app', // <my-app></my-app>
-  providers: [ApiService],
-  directives: [...ROUTER_DIRECTIVES],
-  template: require('./app.component.html'),
-  styles: [require('./app.component.scss')],
+	selector: 'my-app', // <my-app></my-app>
+	providers: [ApiService],
+	template: require('./app.component.html'),
+	styles: [require('./app.component.scss')],
 })
-@RouteConfig([
-  {path: '/', component: HomeComponent, name: 'Home'},
-  {path: '/About', component: AboutComponent, name: 'About'}
-])
-export class AppComponent {
-  url = 'https://github.com/preboot/angular2-webpack';
 
-  constructor(private api: ApiService) {
-  }
+export class AppComponent {
+	url = 'https://github.com/preboot/angular2-webpack';
+
+	constructor(private api: ApiService) {
+	}
 }
