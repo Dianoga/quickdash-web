9cf5d50dff70545b917b6c7b4ddec9a0a06e6303
Brian Steere <brian.steere@smartthings.com>
2016-07-18 18:02:51 -0500

    Redesign for black/white

--- src/app/widgets/climate/climate.component.ts
+++ src/app/widgets/climate/climate.component.ts
@@ -8,7 +8,28 @@ import { DeviceService, Device } from '../../shared';
 	styles: [require('./climate.component.scss')]
 })
 export class ClimateComponent {
+	iconMap: Object = {
+		clear: 'w-clear',
+		cloudy: 'w-cloudy',
+		flurries: 'w-flurries',
+		fog: 'w-fog',
+		hazy: 'w-hazy',
+		mostlycloudy: 'w-partlycloudy',
+		mostlysunny: 'w-partlycloudy',
+		partlycloudy: 'w-partlycloudy',
+		partlysunny: 'w-partlycloudy',
+		rain: 'w-rain',
+		sleet: 'w-sleet',
+		snow: 'w-snow',
+		sunny: 'w-clear',
+		tstorms: 'w-tstorms',
+		unknown: 'w-unknown'
+	};
+
+	outdoorWeather: Device;
+
 	constructor(private devices: DeviceService) {
+		this.outdoorWeather = this.devices.getOutdoorWeather();
 	}
 
 	getDevices(): Device[] {
